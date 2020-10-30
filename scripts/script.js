d9dc455bf1d16cbc8ff87b527c8caf2d6480c595
yorickleblanc <yorick.de.wit@live.nl>
2020-10-30 13:45:05 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-// JavaScript Document
+var deButton = document.querySelector("article + div button ");
+
+deButton.addEventListener("click", klikkie);
+
+function klikkie() {
+    deButton.classList.add("donker");
+}
